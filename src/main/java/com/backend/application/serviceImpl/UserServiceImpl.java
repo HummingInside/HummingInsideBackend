8683362c2b70ab171aca5f980fda925329092a4b
SimHongSub <ghdtjq2038@gmail.com>
2020-09-24 18:40:22 +0900

    [serviceImpl] add api service implementation classes

---
+++ src/main/java/com/backend/application/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.backend.application.serviceImpl;
+
+import com.backend.application.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
