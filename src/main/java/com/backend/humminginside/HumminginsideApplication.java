461e8696db172b4276036622d62266ba050ad70f
cupjoo <cupjoo@naver.com>
2020-09-24 16:05:39 +0900

    second commit

---
+++ src/main/java/com/backend/humminginside/HumminginsideApplication.java
@@ -0,0 +1,13 @@
+package com.backend.humminginside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumminginsideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumminginsideApplication.class, args);
+    }
+
+}
