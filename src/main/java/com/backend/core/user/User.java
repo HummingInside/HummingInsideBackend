cf8f43c4644cef4fc8253784357c28f9459d0212
Unknown <cupjoo@naver.com>
2020-10-01 17:06:02 +0900

    [core] add User domain

--- src/main/java/com/backend/core/user/User.java
+++ src/main/java/com/backend/core/user/User.java
@@ -1,4 +1,28 @@
 package com.backend.core.user;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Getter
+@NoArgsConstructor
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Builder
+    public User(String name){
+        this.name = name;
+    }
 }
