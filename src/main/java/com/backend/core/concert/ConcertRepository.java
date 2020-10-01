e037657b8d071436ac34d16768e8e823da0f19f6
Unknown <cupjoo@naver.com>
2020-10-01 17:06:40 +0900

    [core] add Concert domain

--- src/main/java/com/backend/core/concert/ConcertRepository.java
+++ src/main/java/com/backend/core/concert/ConcertRepository.java
@@ -2,5 +2,5 @@ package com.backend.core.concert;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConcertRepository extends JpaRepository<Concert, String> {
+public interface ConcertRepository extends JpaRepository<Concert, Long> {
 }
