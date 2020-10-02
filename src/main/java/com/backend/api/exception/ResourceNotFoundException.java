de196c737d2b8c3ce9eabb16b701b7d3e1dc009e
Unknown <cupjoo@naver.com>
2020-10-02 18:18:09 +0900

    [exception] implement concert api exceptions

--- src/main/java/com/backend/api/exception/ResourceNotFoundException.java
+++ src/main/java/com/backend/api/exception/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.backend.api.exception;
 
-public class ResourceNotFoundException {
-}
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {}
