600b18e6e370a3f7a008a816517e5963dbe144c0
Unknown <cupjoo@naver.com>
2020-10-02 16:22:24 +0900

    [dto] add conert dtos

---
+++ src/main/java/com/backend/application/dto/concert/ConcertDetailResponse.java
@@ -0,0 +1,37 @@
+package com.backend.application.dto.concert;
+
+import com.backend.core.concert.Category;
+import com.backend.core.concert.Concert;
+import com.backend.core.concert.ConcertStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@NoArgsConstructor
+@Getter
+public class ConcertDetailResponse extends ConcertSimpleResponse {
+    private CategorySimple category;
+    private ConcertStatus status;
+
+    public ConcertDetailResponse(Concert concert){
+        super(concert);
+        category = new CategorySimple(concert.getCategory());
+        status = concert.getStatus();
+    }
+}
+
+@Setter
+@NoArgsConstructor
+@Getter
+class CategorySimple {
+    private Long id;
+    private String name;
+
+    public CategorySimple(Category category){
+        id = category.getId();
+        name = category.getName();
+    }
+}
---
+++ src/main/java/com/backend/application/dto/concert/ConcertSimpleResponse.java
@@ -0,0 +1,35 @@
+package com.backend.application.dto.concert;
+
+import com.backend.core.concert.Category;
+import com.backend.core.concert.Concert;
+import com.backend.core.concert.ConcertStatus;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@NoArgsConstructor
+@Getter
+public class ConcertSimpleResponse {
+    private Long id;
+    private String title;
+    private String performer;
+    private LocalDateTime date;
+    private String description;
+    private int maxAudience;
+    private int price;
+    private int likesCount;
+
+    public ConcertSimpleResponse(Concert concert){
+        id = concert.getId();
+        title = concert.getTitle();
+        performer = concert.getPerformer().getName();
+        date = concert.getDate();
+        description = concert.getDescription();
+        maxAudience = concert.getMaxAudience();
+        price = concert.getPrice();
+        likesCount = concert.getLikesCount();
+    }
+}
