ad8acdb8bf9bbaa2b6982efb6dc245b974df1faa
SimHongSub <ghdtjq2038@gmail.com>
2020-10-07 13:59:52 +0900

    [login] delete User obejct, service and api

--- src/main/java/com/backend/application/serviceImpl/UserServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.backend.application.serviceImpl;
-
-import com.backend.application.service.UserService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserServiceImpl implements UserService {
-}
--- src/main/java/com/backend/core/user/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.backend.core.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
