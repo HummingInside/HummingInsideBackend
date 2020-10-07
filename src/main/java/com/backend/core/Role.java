1583390283aaf7f085439c788510559829c5493c
SimHongSub <ghdtjq2038@gmail.com>
2020-10-07 14:02:06 +0900

    [login] add Role enum class

---
+++ src/main/java/com/backend/core/Role.java
@@ -0,0 +1,13 @@
+package com.backend.core;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum  Role {
+    ADMIN("ROLE_ADMIN"),
+    MEMBER("ROLE_MEMBER");
+
+    private String value;
+}
