fa5d923750df91452cf2572c882c344f00bac7db
myungsegyo <myungsekyo@gmail.com>
2020-10-16 22:28:48 +0900

    [socket] change socket endpoints

--- src/main/java/com/backend/config/WebSocketConfig.java
+++ src/main/java/com/backend/config/WebSocketConfig.java
@@ -11,8 +11,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/listen");
+        config.setApplicationDestinationPrefixes("/send");
     }
 
     @Override
