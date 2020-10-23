0cb2908d8c40995c61c6ad36f971df7af4e37c91
SimHongSub <ghdtjq2038@gmail.com>
2020-10-23 21:48:37 +0900

    [auth] modify member api

--- src/main/java/com/backend/api/MemberApi.java
+++ src/main/java/com/backend/api/MemberApi.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -41,7 +42,9 @@ public class MemberApi {
     }
 
     @PostMapping("/signin")
-    public String dispLogin(@RequestBody Map<String, String> user) {
+    public HashMap<String, String> dispLogin(@RequestBody Map<String, String> user) {
+        HashMap<String, String> result = new HashMap<String, String>();
+
         Member member = memberRepository.findByEmail(user.get("email"))
                 .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
         /*if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
@@ -50,7 +53,15 @@ public class MemberApi {
         if (!user.get("password").equals(member.getPassword())) {
             throw new IllegalArgumentException("잘못된 비밀번호입니다.");
         }
-        return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
+
+        result.put("id", member.getId().toString());
+        result.put("username", member.getUsername());
+        result.put("email", member.getEmail());
+        result.put("role", member.getRoles().toString());
+        result.put("token", jwtTokenProvider.createToken(member.getUsername(), member.getRoles()));
+
+        return result;
+        //return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
     }
 
 }
