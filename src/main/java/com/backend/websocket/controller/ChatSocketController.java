2ed7ec5b67d3c5ad37b576c2cc1d125b5040c088
myungsegyo <myungsekyo@gmail.com>
2020-10-23 20:40:32 +0900

    [socket] add ChatSocketController

---
+++ src/main/java/com/backend/websocket/controller/ChatSocketController.java
@@ -0,0 +1,15 @@
+package com.backend.websocket.controller;
+
+import com.backend.websocket.message.SocketMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatSocketController {
+    @MessageMapping("/{concert_pk}/chat")
+    @SendTo("/listen/{concert_pk}/chat")
+    public SocketMessage forwarding(SocketMessage message) throws Exception {
+        return new SocketMessage(message.getMessage());
+    }
+}
