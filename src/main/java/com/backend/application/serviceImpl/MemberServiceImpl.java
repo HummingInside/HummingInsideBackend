aac03333ee89c8ec68a2524d078e8ac39a18e7ee
SimHongSub <ghdtjq2038@gmail.com>
2020-10-30 21:24:38 +0900

    [auth] modify member service

--- src/main/java/com/backend/application/serviceImpl/MemberServiceImpl.java
+++ src/main/java/com/backend/application/serviceImpl/MemberServiceImpl.java
@@ -1,39 +1,18 @@
 package com.backend.application.serviceImpl;
 
-import com.backend.application.dto.member.MemberCreateRequest;
 import com.backend.application.service.MemberService;
-import com.backend.core.member.Member;
 import com.backend.core.member.MemberRepository;
-import com.backend.core.Role;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
-
-    /*@Transactional
-    public Long joinUser(MemberCreateRequest request){
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        request.setPassword(passwordEncoder.encode(request.getPassword()));
-
-        return memberRepository.save(request.toEntity()).getId();
-    }*/
-
+    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return memberRepository.findByEmail(username)
