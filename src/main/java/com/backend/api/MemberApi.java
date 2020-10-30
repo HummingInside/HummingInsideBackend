0f458f91d135406e250760b79efc217298a1cc0b
SimHongSub <ghdtjq2038@gmail.com>
2020-10-30 21:22:03 +0900

    [auth] add return user info method to check auth

--- src/main/java/com/backend/api/MemberApi.java
+++ src/main/java/com/backend/api/MemberApi.java
@@ -5,12 +5,15 @@ import com.backend.application.service.MemberService;
 import com.backend.core.member.Member;
 import com.backend.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -47,9 +50,7 @@ public class MemberApi {
 
         Member member = memberRepository.findByEmail(user.get("email"))
                 .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-MAIL 입니다."));
-        /*if (!passwordEncoder.matches(user.get("password"), member.getPassword())) {
-            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
-        }*/
+
         if (!user.get("password").equals(member.getPassword())) {
             throw new IllegalArgumentException("잘못된 비밀번호입니다.");
         }
@@ -58,10 +59,27 @@ public class MemberApi {
         result.put("username", member.getUsername());
         result.put("email", member.getEmail());
         result.put("role", member.getRoles().toString());
-        result.put("token", jwtTokenProvider.createToken(member.getUsername(), member.getRoles()));
+        result.put("token", jwtTokenProvider.createToken(member.getEmail(), member.getRoles()));
 
         return result;
         //return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
+    }
+
+    @GetMapping("/user")
+    public HashMap<String, String> getUserInfo() {
+
+        HashMap<String, String> result = new HashMap<String, String>();
+        Authentication authentication =  SecurityContextHolder.getContext().getAuthentication();
+
+        Member member = (Member) authentication.getPrincipal();
+
+        result.put("id", member.getId().toString());
+        result.put("username", member.getUsername());
+        result.put("email", member.getEmail());
+        result.put("role", member.getRoles().toString());
+        result.put("token", jwtTokenProvider.createToken(member.getEmail(), member.getRoles()));
+
+        return result;
     }
 
 }
