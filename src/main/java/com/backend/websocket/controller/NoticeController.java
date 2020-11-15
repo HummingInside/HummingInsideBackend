653f9556b12f0af12e080b5f373877a8cdd6d7b9
myungsegyo <myungsekyo@gmail.com>
2020-11-15 20:37:29 +0900

    [socket] renamed all socket controllers

--- src/main/java/com/backend/websocket/controller/NoticeController.java
+++ src/main/java/com/backend/websocket/controller/NoticeController.java
@@ -6,9 +6,9 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class ChatSocketController {
-    @MessageMapping("/{concert_pk}/chat")
-    @SendTo("/listen/{concert_pk}/chat")
+public class NoticeController {
+    @MessageMapping("/{concert_pk}/notice")
+    @SendTo("/listen/{concert_pk}/notice")
     public SocketMessage forwarding(SocketMessage message) throws Exception {
         return new SocketMessage(message.getMessage());
     }
