8fe46f3e331bc5cfd9229a37632fcf5cfad84a9d
SimHongSub <ghdtjq2038@gmail.com>
2021-03-12 12:23:51 +0900

    [domain] create exception package

--- src/main/java/com/humming/exception/ResourceNotFoundException.java
+++ src/main/java/com/humming/exception/ResourceNotFoundException.java
@@ -1,7 +1,9 @@
-package com.humming.api.exception;
+package com.humming.exception;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
+@RequiredArgsConstructor
 public class ResourceNotFoundException extends RuntimeException {}
